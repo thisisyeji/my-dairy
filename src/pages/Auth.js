ca01d1a344465ebc38ff2bd413b447a3ff5a3698
yjkim0109 <yjkkim0109@gmail.com>
2022-11-16 13:36:27 +0900

    Feat: 구글 로그인 기능

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import AuthForm from '../components/AuthForm';
 import MyButton from '../components/MyButton';
+import { authService } from '../fbase';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 const Auth = () => {
+	const onGoogle = () => {
+		const provider = new GoogleAuthProvider();
+		const data = signInWithPopup(authService, provider);
+	};
 	return (
 		<>
 			<AuthForm />
-			<MyButton text={'Google로 계속하기'} />
+			<MyButton text={'Google로 계속하기'} onClick={onGoogle} />
 		</>
 	);
 };
