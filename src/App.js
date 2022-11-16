f70112253548fef701082e5c953e9dc54ba99708
yjkim0109 <yjkkim0109@gmail.com>
2022-11-16 13:13:24 +0900

    Feat: firebase 및 환경변수 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Edit from './pages/Edit';
+import Auth from './pages/Auth';
+import Diary from './pages/Diary';
+import Home from './pages/Home';
+import New from './pages/New';
+import Nav from './components/Nav';
+import DiaryList from './pages/DiaryList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isLoggedin, setIsLoggedin] = useState(false);
+	return (
+		<BrowserRouter>
+			<div className='App'>
+				<Routes>
+					{isLoggedin ? (
+						<>
+							<Nav />
+							<Route path='/' element={<Home />} />
+							<Route path='/diarylist' element={<DiaryList />} />
+							<Route path='/new' element={<New />} />
+							<Route path='/edit/:id' element={<Edit />} />
+							<Route path='/diary/:id' element={<Diary />} />
+						</>
+					) : (
+						<Route path='/' element={<Auth />} />
+					)}
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
